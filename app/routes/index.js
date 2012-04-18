749af7d643b68b12013ed94cc5aeb7d290063654
Zack Michener <zack@zackmichener.com>
2012-04-18 17:36:37 -0400

    changed view engine to ejs, added add_fortune route

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,3 +7,7 @@ exports.index = function(req, res){
   // res.render('index', { title: 'Express' })
   res.send('sup.');
 };
+
+exports.add_fortune = function(req, res) {
+	res.render('add-fortune', {});
+};
